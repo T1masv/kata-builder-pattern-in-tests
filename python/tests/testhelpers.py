4d0a7c919af9c2cde4c0cb27564cc4204d4ec24f
Clément Caquet <113350471+Clementcqt@users.noreply.github.com>
2024-11-13 11:01:54 +0100

    Update testhelpers.py

--- python/tests/testhelpers.py
+++ python/tests/testhelpers.py
@@ -1,6 +1,6 @@
 from Shop import User
 
-def create_user_with_age_verified(age:int=25, vefified:bool=True, name:str="bob", email:str="bob@gmail.com", address:str="1 boulevard jean jaures"):
+def create_user(age:int=25, vefified:bool=True, name:str="bob", email:str="bob@gmail.com", address:str="1 boulevard jean jaures"):
   return User(
       name=name,
       email=email,
