e13b7e583c5035f594f7712067190e5368c6ed9a
Clément Caquet <caquetclement2002@gmail.com>
2024-11-13 11:41:08 +0100

    test_shop updated

--- python/tests/test_shop.py
+++ python/tests/test_shop.py
@@ -2,18 +2,18 @@ from shop import Shop
 from tests.conftest import create_user
 
 def test_happy_path():
-    user = create_user(has_address_local=True)
+    user = create_user()
     assert Shop.can_order(user)
     assert not Shop.must_pay_foreign_fee(user)
 
 
 def test_minors_cannot_order_from_the_shop():
-    user = create_user(has_address_local=True, is_major=False, is_verified=True)
+    user = create_user(is_major=False)
     assert not Shop.can_order(user)
 
 
 def test_cannot_order_if_not_verified():
-    user = create_user(has_address_local=True, is_major=True, is_verified=False)
+    user = create_user(is_verified=False)
     assert not Shop.can_order(user)
 
 
