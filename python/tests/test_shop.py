622049513261ce693a9cf4789bff0d24be91999f
Clément Caquet <113350471+Clementcqt@users.noreply.github.com>
2024-11-13 11:01:25 +0100

    Update test_shop.py

--- python/tests/test_shop.py
+++ python/tests/test_shop.py
@@ -2,49 +2,21 @@ from shop import Shop, User
 
 
 def test_happy_path(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=fsf_address,
-        verified=True,
-    )
-
+    user = create_user(address=fsf_address)
     assert Shop.can_order(user)
     assert not Shop.must_pay_foreign_fee(user)
 
 
 def test_minors_cannot_order_from_the_shop(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=16,
-        address=fsf_address,
-        verified=True,
-    )
-
+    user = create_user(address=fsf_address, age=16, verified=True)
     assert not Shop.can_order(user)
 
 
 def test_cannot_order_if_not_verified(fsf_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=16,
-        address=fsf_address,
-        verified=False,
-    )
-
+    user = create_user(address=fsf_address, age=25, verified=False)
     assert not Shop.can_order(user)
 
 
 def test_foreigners_must_be_foreign_fee(paris_address):
-    user = User(
-        name="bob",
-        email="bob@domain.tld",
-        age=25,
-        address=paris_address,
-        verified=False,
-    )
-
+    user = create_user(address=fsf_address)
     assert Shop.must_pay_foreign_fee(user)
